d77b49d1946f17032382d20a75281e2cf18dffb1
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-22 20:02:44 +0530

    #include<iostream> #include<math.h> #include<iomanip> using namespace std; int main() {     float x1,y1,x2,y2;     cin >>x1>>y1>>x2>>y2;     cout <<fixed<<setprecision(4);     cout <<sqrt(pow(x2-x1,2)+pow(y2-y1,2)); }

---
+++ Distance_between_two_points.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float x1,y1,x2,y2;
+    cin >>x1>>y1>>x2>>y2;
+    cout <<fixed<<setprecision(4);
+    cout <<sqrt(pow(x2-x1,2)+pow(y2-y1,2));
+}
