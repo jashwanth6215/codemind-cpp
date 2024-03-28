99913113650d4c5cbcf4719e14b8d36e75340f66
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-28 20:21:19 +0530

    Add files via upload

---
+++ Tickets.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >>x;
+    if(x*4<=1000)
+    {
+        cout <<"YES";
+    }
+    else
+    {
+        cout <<"NO";
+    }
+}
