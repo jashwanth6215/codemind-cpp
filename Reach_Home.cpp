8cac36d1c40f7161fb47e4b4d336bd1b1c106b02
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-29 21:23:18 +0530

    Add files via upload

---
+++ Reach_Home.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin >>x>>y;
+    if(5*x>=y)
+    {
+        cout <<"YES";
+    }
+    else
+    {
+        cout <<"NO";
+    }
+}
