94e9c165397eecb50e7e6c60f92e486db0e59990
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-03-29 21:27:32 +0530

    #include<iostream>

---
+++ Good_Investment_or_Not.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin >>x>>y;
+    if(x>=2*y)
+    {
+        cout <<"YES";
+    }
+    else
+    {
+        cout <<"NO";
+    }
+}
