e0d5e7a1280f2f3e4498eb906419eabe07bd0f52
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-02 16:12:02 +0530

    #include<iostream>

---
+++ Target.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p1,p2,p3,p4;
+    cin >>p1>>p2>>p3>>p4;
+    if(p1>=10 && p2>=10 && p3>=10 &&p4>=10)
+    {
+        cout <<"YES";
+    }
+    else
+    {
+        cout <<"NO";
+    }
+}
