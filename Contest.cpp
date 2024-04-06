6fee6047d2bca2fe9d4c8423338cc7ddbc69c131
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-06 22:47:00 +0530

    #include<iostream>

---
+++ Contest.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,a,b;
+    cin >>x>>a>>b;
+    if(a+(2*b)>=x)
+    {
+        cout <<"Qualify";
+    }
+    else
+    {
+        cout <<"Not Qualify";
+    }
+}
