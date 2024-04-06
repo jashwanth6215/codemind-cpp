4ee85e2ceddd15b8aeb359098da851062dde56e9
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-06 22:41:36 +0530

    #include<iostream>

---
+++ Passed_or_Failed.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int e,m,p,ch,c,s;
+    cin >>e>>m>>p>>ch>>c>>s;
+    if(e>34 && m>34 && p>34 && ch>34 && c>34 && s>34)
+    {
+        cout <<"PASSED";
+    }
+    else
+    {
+        cout <<"FAILED";
+    }
+}
