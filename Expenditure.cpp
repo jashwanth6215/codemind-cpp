7a7dd712a0c8e9a9f5b31c37f82a5f92eff0839a
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-07 23:05:29 +0530

    #include<iostream>

---
+++ Expenditure.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cin >>x>>y;
+    if(x>=(y*30))
+    {
+        cout <<"YES";
+    }
+    else
+    {
+        cout <<"NO";
+    }
+}
