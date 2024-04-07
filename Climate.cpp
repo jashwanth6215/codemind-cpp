ba377d17da5702a97acfc9db00d4e7dd088a42ef
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-07 23:12:55 +0530

    #include<iostream>

---
+++ Climate.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int C;
+    cin >>C;
+    if(C>20)
+    {
+        cout <<"HOT";
+    }
+    else
+    {
+        cout <<"COLD";
+    }
+}
