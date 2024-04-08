90099f3d6a89303b5aad4c878d8d4a49dd0a9cbd
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-08 22:43:07 +0530

    #include<iostream>

---
+++ Number_Divisible_with_9.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >>a;
+    if(a%9==0)
+    {
+        cout <<"True";
+    }
+    else
+    {
+        cout <<"False";
+    }
+}
