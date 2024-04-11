bff34299ebe7dc96fed00788599b18b2ec3e54a8
Jaswanth <147476570+jashwanth6215@users.noreply.github.com>
2024-04-11 20:02:29 +0530

    #include<iostream>

---
+++ Find_the_Digits_Count.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,count=0;
+    cin >>n;
+    while(n!=0)
+    {
+        n=n/10;
+        count = count +1;
+    }
+    cout <<count;
+}
